5afb16a852488287d32c7d27840534c069381a36
eliseunogueira <eliseu.nogueira@outlook.com>
2020-03-01 22:25:08 -0300

    add dishdetailscomponent

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -9,6 +9,7 @@ function Menu(props) {
         key={index}
         title={item.name}
         subtitle={item.description}
+        onPress={() => props.onPress(item.id)}
         leftAvatar={{ source: require('./images/zucchipakoda.png') }}
       />
     );
